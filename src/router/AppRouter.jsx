094a0b126d7786d4c20a721ff5176438fe36b1da
Turgay Levent <turgayleventt@gmail.com>
2022-12-02 00:36:13 +0300

    approuter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/nav/Navbar";
+import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
-    <div>
-      App Router
-    </div>
-  )
-}
+    <BrowserRouter>
+      <GlobalStyles />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
