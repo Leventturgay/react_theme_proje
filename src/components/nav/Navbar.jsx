ae6a8f835f405dda0b014355fb7c4d6b52a3155b
Turgay Levent <turgayleventt@gmail.com>
2022-12-02 16:27:38 +0300

    hamburger

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.style";
-import Nav, { Brand, Menu } from "./Navbar.style";
-import MenuLink from "./Navbar.style";
+import Nav, { Brand, Menu, MenuLink, Hamburger } from "./Navbar.style";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Nav justify="space-between" wrap="wrap">
       <Brand to="/">
         <i>Turgay LEVENT </i>
         <span>/ Recipe</span>
       </Brand>
-      <Menu>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </Hamburger>
+      <Menu isOpen={isOpen} onClick={() => setIsOpen(false)}>
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="about">About</MenuLink>
         <MenuLink to="register">Register</MenuLink>
