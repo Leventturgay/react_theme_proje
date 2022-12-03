07406e0bd96ca6459186513185e1f11694c45a7b
Turgay Levent <turgayleventt@gmail.com>
2022-12-03 21:28:40 +0300

    header2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./Header.style";
+import {
+  Button,
+  FoodInput,
+  FormContainer,
+  HeaderContainer,
+  MainHeader,
+  Select,
+} from "./Header.style";
 
-const Header = () => {
+const Header = ({ setQuery, setSelectedMeal, mealType, getData }) => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <HeaderContainer>
+      <MainHeader>RECİPE APP</MainHeader>
+      <FormContainer>
+        <FoodInput
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Button type="submit">SEARCH</Button>
+        <Select
+          name="mealType"
+          id="mealType"
+          onChange={(e) => setSelectedMeal(e.target.value)}
+        >
+          {mealType.map((meal, index) => {
+            return (
+              <option key={index} value={meal}>
+                {meal}
+              </option>
+            );
+          })}
+        </Select>
+      </FormContainer>
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,36 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
+import Header from "../../components/header/Header";
 
 const Home = () => {
   const APP_ID = "08b57ada";
   const APP_KEY = "7b4e1fdc3a5b6e96ec44799ee82ce85c";
   const [query, setQuery] = useState("egg");
   const [selectedMeal, setSelectedMeal] = useState("dinner");
+  const [recipes, setRecipes] = useState([]);
+  const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
   const getData = async () => {
     const { data } = await axios(url);
-    console.log(data);
+    setRecipes(data.hits);
   };
+  console.log(recipes);
   useEffect(() => {
     getData();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header
+        setQuery={setQuery}
+        setSelectedMeal={setSelectedMeal}
+        mealType={mealType}
+        getdata={getData}
+      />
+    </div>
+  );
 };
 
 export default Home;
