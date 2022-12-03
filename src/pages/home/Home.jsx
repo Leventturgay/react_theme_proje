569f5c602d0348ee18f0ac07b40b19d00e67811c
Turgay Levent <turgayleventt@gmail.com>
2022-12-03 16:10:41 +0300

    data1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  const APP_ID = "08b57ada";
+  const APP_KEY = "7b4e1fdc3a5b6e96ec44799ee82ce85c";
+  const [query, setQuery] = useState("egg");
+  const [selectedMeal, setSelectedMeal] = useState("dinner");
 
-export default Home
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+  const getData = async () => {
+    const { data } = await axios(url);
+    console.log(data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div>Home</div>;
+};
+
+export default Home;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/nav/Navbar";
 import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 import About from "../pages/about/About";
 import Detail from "../pages/detail/Detail";
 import PrivateRouter from "./PrivateRouter";
@@ -16,6 +17,8 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+
         <Route path="about" element={<PrivateRouter />}>
           <Route path="" element={<About />} />
         </Route>
